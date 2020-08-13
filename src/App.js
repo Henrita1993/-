e76019e4ec197e94e3fc008f12837c7a4f52c999
lyt <913455233@qq.com>
2020-08-13 19:28:38 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import {BrowserRouter,Switch,Route} from "react-router-dom"
+import Login from "./pages/login/login.jsx"
+import Admin from "./pages/admin/admin.jsx"
+// import { Button } from "antd"
+// import { Button, Tooltip } from 'antd';
+// import { SearchOutlined } from '@ant-design/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import "./App.less"
+
+export default class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+      <Switch>
+      <Route path="/" component={Login}/> 
+      <Route path="/admin" component={Admin}/>
+      </Switch>
+     
+      </BrowserRouter>
+    )
+  }
+
 }
 
-export default App;
+
+
+
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,102 @@
+import React,{Component} from "react"
+import { Form, Input, Button, Checkbox } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import "./login.less"
+import logo from "./imgs/logo.png"
+const Item = Form.Item
+
+
+export default class Login extends Component{
+  handleSubmit=()=>{
+  console.log("i submit")
+  }
+ 
+  render () {
+    // 得到组件值
+      const onFinish = values => {
+        console.log('Received values of form: ', values);
+      };
+
+    return (
+      <div className="login">
+     
+        <header className="header">
+        <img src={logo} alt="logo"/>
+        <h1>商品管理系统</h1>
+        </header>
+
+        <section className="content">
+        <h3>用户登录</h3>
+        <Form name="normal_login" 
+        className="login-form"   
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        >
+          <Item 
+          name="username"
+           /*
+                用户名/密码的的合法性要求
+                  1). 必须输入
+                  2). 必须大于等于4位
+                  3). 必须小于等于12位
+                  4). 必须是字母、数字、下划线组成
+                */
+          rules={[
+            {required: true, message: '用户名必须输入'},
+            {min:4,message:"必须大于等于4位"},
+            {max:12,message:"必须小于等于12位"},
+            {pattern:/^\w+$/,message:"必须是字母、数字、下划线组成"}
+          
+          ]}
+          >
+          <Input prefix={<UserOutlined className="site-form-item-icon" style={{color:"rgba(0,0,0,.25)"}} />} placeholder="用户名" />
+
+          </Item>
+         
+          <Item 
+          name="password"
+          rules={[
+            {required: true, message: '密码必须输入'},
+            {min:4,message:"必须大于等于4位"},
+            {max:12,message:"必须小于等于12位"},
+            {pattern:/^\w+$/,message:"必须是字母、数字、下划线组成"}
+          
+          ]}
+          >
+          <Input
+          prefix={<LockOutlined className="site-form-item-icon" style={{color:"rgba(0,0,0,.25)"}}/>}
+          type="password"
+          placeholder="Password"
+        />
+          </Item>
+
+        <Item>  
+        <Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>记住我</Checkbox>
+        </Item>
+        <a className="login-form-forgot" href="">
+        忘记密码
+        </a>
+        </Item>
+         
+
+          <Item className="form-bottom">
+          <Button type="primary" htmlType="submit" className="login-button">
+            登录
+          </Button>
+        
+          </Item>
+      
+
+        </Form>
+        
+        </section>
+
+    </div>
+
+    ) 
+  }
+
+}
